7f3cbbcc9ec0ff3e984a4a8f614f12d6b9de74d4
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-30 19:10:19 -0500

    started on a test. will finish later

--- test/test.model.js
+++ test/test.model.js
@@ -2,3 +2,10 @@
 const mocha = require('mocha');
 const { assert } = require('chai');
 
+describe("model function tests", ()=>{
+  // getAllFunctions
+  let { getAllTrainingPrograms } = require('../model');
+  it("should return all the training programs listed in the database",()=>{
+
+  });
+});
