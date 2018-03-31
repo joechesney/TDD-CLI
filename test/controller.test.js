67565afa45f096939d45b32e3b5f6ee99755965f
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-31 16:32:29 -0500

    added before statement to controller tests

--- test/controller.test.js
+++ test/controller.test.js
@@ -3,11 +3,24 @@ const { assert: { isObject, isArray } } = require('chai');
 
 describe("controller function tests", ()=>{
 
+  before(()=>{
+    let newTrainingProgram = {
+      no_of_seats: 30,
+      instructor_name: "Gerrard",
+      start_date: "March 31",
+      end_date: "never",
+      course_category: "suffering"
+    } // use this to test the updateAdd controller function maybe?
+  });
+  
   // getAllFunctions
   it("should return all the training programs listed in the database",()=>{
     let { getAllTrainingPrograms } = require('../controllers/controller');
-    getAllTrainingPrograms().
-    then(tps=>isArray(tps));
+    getAllTrainingPrograms()
+    .then(tps=>{
+      isArray(tps);
+      isObject(tps[1]);
+    });
   });
 
   // getOne function
@@ -17,11 +30,5 @@ describe("controller function tests", ()=>{
     .then(tp=> isObject(tp));
   });
 
-  let newTrainingProgram = {
-    no_of_seats: 30,
-    instructor_name: "Gerrard",
-    start_date: "March 31",
-    end_date: "never",
-    course_category: "suffering"
-  } // use this to test the updateAdd controller function maybe?
+
 });
