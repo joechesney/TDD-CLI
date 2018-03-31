49a55c8ce8a8d222102fce38c1ebefbc52dbfb15
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-31 15:31:20 -0500

    added test to make sure the model functions are functions

--- test/test.model.js
+++ test/test.model.js
@@ -5,7 +5,15 @@ const { assert } = require('chai');
 describe("MODEL FUNCTION TESTS ", ()=>{
   
   let { getAll, getOne, deleteOne, updateAdd } = require('../models/model');
-  it("getAll should return a promise",()=>{
+  
+  it("all model functions should be functions",()=>{
+    assert.isFunction(getAll, "function");
+    assert.isFunction(getOne, "function");
+    assert.isFunction(deleteOne, "function");
+    assert.isFunction(updateAdd, "function");
+  });
+
+  it("all model functions should return a promise",()=>{
     assert.typeOf(getAll().catch(err=>{}), "promise");
     assert.typeOf(getOne(5).catch(err=>{}), "promise");
     assert.typeOf(deleteOne(6).catch(err=>{}), "promise");
