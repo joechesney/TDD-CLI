0106ef69171844cd9223d3918428fe64cf0afd5b
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-01 16:14:52 -0500

    required in prompt and tested it a lil bit using the official docs boilerplate

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 
 const {createDatabase} = require('./db/build-db');
-
+const prompt = require('prompt');
 createDatabase();
+
+prompt.start();
+
+const schema = {
+  properties: {
+    name:{
+      pattern: /^[a-zA-Z\s\-]+$/,
+      message: "name must only be leters, spaces, or dashes, bruh",
+      required: true
+    },
+    password:{
+      hidden: true
+    }
+  },
+};
+
+prompt.get(schema, (err, result)=>{
+  console.log('input received');
+  console.log( result.name);
+  console.log( result.password);
+});
